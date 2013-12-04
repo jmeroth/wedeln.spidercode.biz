eed2adb8e3ed8db296d8d43e2bb241f724802fdd
jmeroth <john.meroth@state.ma.us>
2013-12-03 23:32:10 -0500

    add div click-event code

--- views/v_index_index.php
+++ views/v_index_index.php
@@ -5,6 +5,7 @@
 	This message is being triggered via the c_index.php controller, within the index() method.
 </p>
 
+
 <div id='tune1' class='music'>Tune 1</div>
 <div id='tune2' class='music'>Tune 2</div>
 <div id='tune3' class='music'>Tune 3</div>
@@ -15,14 +16,37 @@
 	<option>red</option>
 	<option>blue</option>
 	<option>yellow</option>
+	<option>green</option>
 </select>
 <input type="reset">
+
 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
+
 <script>
+	alert("Hi there.");
+
+	$('#tune1').click(function() {   
+        console.log("Hello World.");	
+		$('#tune1').css('background-color',$( "#color" ).val());
+    });
+
+	$('#tune2').click(function() {   
+        console.log("Hello World.");	
+		$('#tune2').css('background-color',$( "#color" ).val());
+    });
+
+	$('#tune3').click(function() {   
+        console.log("Hello World.");	
+		$('#tune3').css('background-color',$( "#color" ).val());
+    });
+
+	$('#tune4').click(function() {   
+        console.log("Hello World.");	
+		$('#tune4').css('background-color',$( "#color" ).val());
+    });
 
 	$('input').click(function() {   
         console.log("Hello World.");
 		$('body').css('background-color',$( "#color" ).val());
     });
-
 </script>
