6955a0c9301cf0ef93caa9dbba441f2e7ef34df1
jmeroth <jmeroth@alum.rpi.edu>
2014-05-17 16:39:45 -0400

    added v_login file from p2

---
+++ views/v_users_login.php
@@ -0,0 +1,28 @@
+<form method='POST' action='/users/p_login'>
+
+    Email<br>
+    <input type='text' name='email'>
+    <br><br>
+
+    Password<br>
+    <input type='password' name='password'>
+    <br><br>
+
+	<?php if($error == 'password_error'): ?>
+        <div class='error' style="color: #090; line-height: 1.2">
+            Login failed. Please double check your password.
+        </div>
+        <br>
+    <?php endif; ?>
+
+
+	<?php if($error == 'email_error'): ?>
+        <div class='error' style="color: #090; line-height: 1.2">
+            Login failed. Please double check your email.
+        </div>
+        <br>
+    <?php endif; ?>
+
+    <input type='submit' value='Log in'>
+
+</form>
