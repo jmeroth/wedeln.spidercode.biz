a27de75219450dcf639dc2611a880bcb4490aea4
jmeroth <jmeroth@alum.rpi.edu>
2014-09-19 17:13:39 -0400

    add helloworld.php and update NOTES.txt

---
+++ helloworld.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html>
+<head>
+
+    <title>Hello World</title>
+    <meta charset='utf-8'>
+
+</head>
+<body>
+
+    <?php echo 'Hello World! You are visiting '.$_SERVER['SERVER_NAME']; ?>
+
+</body>
+</html>
